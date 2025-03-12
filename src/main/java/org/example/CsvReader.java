dae4a71ea789e7885f721b870e786349dc4f0523
revathi-chandra23 <revathichandra2302@gmail.com>
2025-03-12 15:19:06 +0530

    main

--- src/main/java/org/example/CsvReader.java
+++ src/main/java/org/example/CsvReader.java
@@ -28,7 +28,7 @@ public class CsvReader {
 
                 Optional<Double> rating = (values.length > 5 && !values[5].isEmpty()) ? Optional.of(Double.parseDouble(values[5])) : Optional.empty();
 
-                readData.add(new Product(Integer.parseInt(values[0]), values[1], values[2], Double.parseDouble(values[3]), values[4].isEmpty() ? 0.0 : Double.parseDouble(values[4]), rating));
+                readData.add(new Product(Integer.parseInt(values[0]), values[1], values[2], Double.parseDouble(values[3]), values[4].isEmpty() ? 0.0 : Double.parseDouble(values[4]), Optional.of(rating.orElse(3.0))));
             }
         } catch (Exception e) {
             throw new filenotfound("file not found" + e);
