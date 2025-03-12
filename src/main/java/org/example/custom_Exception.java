38d344c502aa509b8db84e4e996add5a6e027bf9
revathi-chandra23 <revathichandra2302@gmail.com>
2025-03-12 14:29:08 +0530

    custom exception

--- src/main/java/org/example/custom_Exception.java
+++ src/main/java/org/example/custom_Exception.java
@@ -1,7 +1,7 @@
 package org.example;
 
 public class custom_Exception extends Exception {
-    public custom_Exception(String  message) {
+    public custom_Exception(String message) {
         super(message);
     }
 }
